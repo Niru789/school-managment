2ea5319bae5d5e1a95940d21578c9d6c2a75e33c
nirmala <nirmala.dec226547@gmail.com>
2023-01-11 09:37:07 +0530

    adding initial files

---
+++ school-management/src/main/java/org/example/Student.java
@@ -0,0 +1,18 @@
+package org.example;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+
+    private String name;
+
+    private int id;
+}
