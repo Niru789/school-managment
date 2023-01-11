19c09d4fe02b6775dab92f32ddc12f31e1887a1d
nirmala <nirmala.dec226547@gmail.com>
2023-01-11 09:35:54 +0530

    adding initial files

--- school-management/src/main/java/org/example/Main.java
+++ school-management/src/main/java/org/example/Main.java
@@ -3,5 +3,15 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Student student = new Student();
+        student.setId(9);
+        student.setName("Shrinidhi");
+        System.out.println(student.getName());
+
+        Student student1 = new Student("Basu " ,10);
+
+
+        System.out.println(student1.getName());
     }
 }
